c239f9825c0a02099b299d6a14d23ae53b4b15a4
Parth Kathane <parth.kathane23@pccoepune.org>
2025-05-24 02:31:17 +0530

    Day - 8 - Medium

--- 560. Subarray Sum Equals K.cpp
+++ 560. Subarray Sum Equals K.cpp
@@ -13,3 +13,25 @@
 // Output: 2
 
 
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map <int,int> m;  
+        //sum,count
+        int total = 0;
+        m[0] = 1;
+        int prefix_sum = 0;
+        for (int i = 0; i<nums.size();++i){
+            prefix_sum += nums[i];
+            if (m.count(prefix_sum - k)){
+                total += m[prefix_sum-k];
+                m[prefix_sum]++;
+            }
+            else{
+                m[prefix_sum] ++; 
+                //(6,2) already present now encountered with 6 again but 6-k is not in map in this case we can't set count of 6 to 1, hence use ++ -> (6,3)
+            }
+        }
+        return total;
+    }
+};
