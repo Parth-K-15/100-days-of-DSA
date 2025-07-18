6c42c44e54cd10183a758f7d258d835a61545eda
Parth Kathane <parth.kathane23@pccoepune.org>
2025-07-18 23:50:57 +0530

    Day - 23 - Medium

---
+++ 155. Min Stack.cpp
@@ -0,0 +1,72 @@
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+
+
+
+
+class MinStack {
+public:
+    stack<int> minStack;
+    stack<int> mainStack;
+
+    MinStack() {}
+
+    void push(int val) {
+        if (minStack.empty() || val <= minStack.top()) {
+            minStack.push(val);
+        }
+        mainStack.push(val);
+    }
+
+    void pop() {
+        if (mainStack.top() == minStack.top()) {
+            minStack.pop();
+        }
+        mainStack.pop();
+    }
+
+    int top() { return mainStack.top(); }
+
+    int getMin() {
+        return minStack.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
