ace5d3b276aed6cb642d0d869ee0301cacfe7884
Parth Kathane <parth.kathane23@pccoepune.org>
2025-07-21 00:08:16 +0530

    Day - 26 - Medium

--- 5. Longest Palindromic Substring.cpp
+++ 5. Longest Palindromic Substring.cpp
@@ -35,7 +35,7 @@ public:
             int len2 = right-left-1;
 
             int len = max(len1,len2);
-            if (len>end-start){
+            if (len>end-start+1){
                 start = i - (len-1)/2;
                 end = i + len/2;
             }
