9798b3c5a46b151c38dbee7ef5c375ff60897fc7
Parth Kathane <parth.kathane23@pccoepune.org>
2025-07-29 16:15:56 +0530

    Day - 35 - Medium

--- 239. Sliding Window Maximum.cpp
+++ 239. Sliding Window Maximum.cpp
@@ -22,3 +22,55 @@ Example 2:
 Input: nums = [1], k = 1
 Output: [1]
  
+
+
+
+
+//not optimised (TC constraints)
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int i = 0, start = 0, end = k-1, a = INT_MIN;
+        int n = nums.size();
+        vector <int> ans;
+
+        while(end < n){
+            a = max(a,nums[i]);
+            ++i;
+            if(i>end){
+                ans.push_back(a);
+                end++;start++;i=start;
+                a = INT_MIN;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+//Optimsed
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> de;
+        vector<int> ans;
+        for (int i = 0; i < k - 1; ++i) {
+            while (!de.empty() && nums[i] > nums[de.back()]) {
+                de.pop_back();
+            }
+            de.push_back(i);
+        }
+        for (int i = k-1; i < nums.size(); ++i) {
+            if (!de.empty() && de.front() <= i - k) {
+                de.pop_front();
+            }
+            while (!de.empty() && nums[i] > nums[de.back()]) {
+                de.pop_back();
+            }
+            de.push_back(i);
+            ans.push_back(nums[de.front()]);
+        }
+        return ans;
+    }
+};
