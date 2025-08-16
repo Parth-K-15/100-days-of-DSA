78e0a41d8adad7425fcd84af16a35ccd2a366d04
Parth Kathane <parth.kathane23@pccoepune.org>
2025-08-16 17:01:21 +0530

    Day - 53 - Medium

--- 974. Subarray Sums Divisible by K.cpp
+++ 974. Subarray Sums Divisible by K.cpp
@@ -16,3 +16,46 @@ Input: nums = [5], k = 9
 Output: 0
 
 
+
+
+Time Complexity → O(n2)
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int start = 0, end = 0, ans = 0, n = nums.size();
+        int sum = 0;
+        while (start <= end && end < n){
+            sum += nums[end];
+            if (sum % k == 0){
+                ans++;
+            }
+            if (end == n-1){
+                end=start;start++;
+                sum = 0;
+            }
+            end++;
+        }
+        return ans;
+    }
+};
+
+
+Time Complexity → O(n)
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int ans = 0, n = nums.size();
+        int PrefixSum = 0;
+        unordered_map <int, int> m;
+        m[0] = 1;
+        for (int i = 0; i<n; ++i){
+            PrefixSum += nums[i];
+            int mod = (PrefixSum % k + k) % k; // Ensure mod is non-negative
+            if (m.count(mod)) {
+                ans += m[mod];
+            }
+            m[mod]++;
+        }
+        return ans;
+    }
+};
