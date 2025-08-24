ec03bf483a539039a5d68507cfb6a1ced2b7398c
Parth Kathane <parth.kathane23@pccoepune.org>
2025-08-24 18:37:04 +0530

    Day - 59 - Medium

---
+++ GFG_Kth Smallest.cpp
@@ -0,0 +1,34 @@
+Given an array arr[] and an integer k where k is smaller than the size of the array, your task is to find the kth smallest element in the given array.
+
+Follow up: Don't solve it using the inbuilt sort function.
+
+Examples :
+
+Input: arr[] = [7, 10, 4, 3, 20, 15], k = 3
+Output: 7
+Explanation: 3rd smallest element in the given array is 7.
+Input: arr[] = [2, 3, 1, 20, 15], k = 4 
+Output: 15
+Explanation: 4th smallest element in the given array is 15.
+
+
+
+
+
+// User function template for C++
+
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k) {
+        // code here
+        priority_queue<int,vector<int>,greater<int>>pq(arr.begin(),arr.end());
+        while(k>1){
+            int a = pq.top();
+            pq.pop();
+            k--;
+        }
+        return pq.top();
+    }
+};
