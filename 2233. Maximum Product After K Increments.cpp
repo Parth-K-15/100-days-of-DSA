268bbbdd2ef772f9558077a7bf0860ed022be8b4
Parth Kathane <parth.kathane23@pccoepune.org>
2026-06-30 11:44:21 +0530

    Day - 94 - Medium

---
+++ 2233. Maximum Product After K Increments.cpp
@@ -0,0 +1,53 @@
+You are given an array of non-negative integers nums and an integer k. In one operation, you may choose any element from nums and increment it by 1.
+
+Return the maximum product of nums after at most k operations. Since the answer may be very large, return it modulo 109 + 7. Note that you should maximize the product before taking the modulo. 
+
+ 
+
+Example 1:
+
+Input: nums = [0,4], k = 5
+Output: 20
+Explanation: Increment the first number 5 times.
+Now nums = [5, 4], with a product of 5 * 4 = 20.
+It can be shown that 20 is maximum product possible, so we return 20.
+Note that there may be other ways to increment nums to have the maximum product.
+Example 2:
+
+Input: nums = [6,3,3,2], k = 2
+Output: 216
+Explanation: Increment the second number 1 time and increment the fourth number 1 time.
+Now nums = [6, 4, 3, 3], with a product of 6 * 4 * 3 * 3 = 216.
+It can be shown that 216 is maximum product possible, so we return 216.
+Note that there may be other ways to increment nums to have the maximum product.
+ 
+
+Constraints:
+
+1 <= nums.length, k <= 105
+0 <= nums[i] <= 106
+
+
+
+
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums, int k) {
+        if(nums.empty()) return 0;
+        priority_queue<int,vector<int>,greater<int>> pq (nums.begin(), nums.end());
+        int temp = 0;
+        for (int i = 0; i<k; ++i){
+            temp = pq.top()+1;
+            pq.pop();
+            pq.push(temp);
+        }
+        long long ans = 1;
+        int MOD  = 1e9+7;
+        while(!pq.empty()){
+            ans = (ans*pq.top())%MOD;
+            pq.pop();
+        }
+        return ans;
+    }
+};
