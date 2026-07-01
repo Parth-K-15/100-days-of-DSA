b7314e02f62517b2507a448f74a988b15f91089e
Parth Kathane <parth.kathane23@pccoepune.org>
2026-07-01 14:23:09 +0530

    Day - 97 - Medium

---
+++ 39. Combination Sum.cpp
@@ -0,0 +1,64 @@
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+ 
+
+Constraints:
+
+1 <= candidates.length <= 30
+2 <= candidates[i] <= 40
+All elements of candidates are distinct.
+1 <= target <= 40
+
+
+
+
+
+
+
+class Solution {
+public:
+    void Find(vector<int>& candidates, int target, vector<vector<int>> &ans, int index, vector<int>&temp){
+        if(target == 0 && index ==candidates.size()) {
+            ans.push_back(temp);
+        }
+        if(index == candidates.size() || target < 0){
+            return;
+        }
+        //include
+        Find(candidates, target, ans, index+1, temp);
+        temp.push_back(candidates[index]);
+        Find(candidates, target-candidates[index], ans, index, temp);
+        temp.pop_back();
+        //don't include again
+
+    }   
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans={};
+        vector<int> temp;
+        Find(candidates, target, ans, 0,temp);
+        return ans;
+    }
+};
