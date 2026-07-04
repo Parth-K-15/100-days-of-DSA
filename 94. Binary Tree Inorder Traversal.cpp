c90b171a7fd556371bdb93872e125b3627f7dc6e
Kathane Parth <parth.kathane23@pccoepune.org>
2026-07-04 14:02:03 +0530

    Day - 103 - Easy

--- 94. Binary Tree Inorder Traversal.cpp
+++ 94. Binary Tree Inorder Traversal.cpp
@@ -8,6 +8,58 @@ Input: root = [1,null,2,3]
 
 Output: [1,3,2]
 
+Explanation:
 
 
-  
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [4,2,6,5,7,1,3,9,8]
+
+Explanation:
+
+
+
+Example 3:
+
+Input: root = []
+
+Output: []
+
+Example 4:
+
+Input: root = [1]
+
+Output: [1]
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void  inorder(TreeNode* root, vector<int>&ans){
+        if(!root) return;
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        inorder(root,ans);
+        return ans;
+    }
+};
