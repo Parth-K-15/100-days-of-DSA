3ec9f3a68622c2318341a360e1e4e1b9a036ab59
Kathane Parth <parth.kathane23@pccoepune.org>
2026-07-28 21:09:45 +0530

    Day - 109 - Easy

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -35,14 +35,14 @@ Constraints:
 
 class Solution {
 public:
-    int fib(int n) {
-        if(n<=1)return n;
-        int prev2 = 0, prev = 1;
+    int climbStairs(int n) {
+        if(n<=1) return n;
+        int prev2 = 1, prev = 1;
         int curr;
-        for (int i = 2; i<=n; ++i){
+        for (int i =2; i<=n; ++i){
             curr = prev2+prev;
-            prev2 = prev;
-            prev = curr;
+            prev2=prev;
+            prev=curr;
         }
         return curr;
     }
