31b68b660ed861fc952138b0633b3e66c7350364
Kathane Parth <parth.kathane23@pccoepune.org>
2026-07-30 16:55:54 +0530

    Day - 114 - Medium

---
+++ 213. House Robber II.cpp
@@ -0,0 +1,94 @@
+
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+Example 2:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 3
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 100
+// 0 <= nums[i] <= 1000
+
+
+
+
+//Accepted top down approach solution but can be further optimised
+class Solution {
+public:
+    int steal(int n, vector<int>&nums, vector<int>& dp) {
+        if (n < 0)
+            return 0;
+        if (n == 0)
+            return nums[0];
+        if (dp[n] != -1)
+            return dp[n];
+        return dp[n] = max(steal(n - 1, nums, dp),
+                           nums[n] + steal(n - 2, nums, dp));
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        vector<int> dp(n + 1, -1), dp2(n + 1, -1);
+        vector<int> nums2 = nums;
+        nums[0] = 0;
+        nums2[n-1] = 0;
+        return max(steal(n - 1, nums, dp), steal(n - 1, nums2, dp2));
+        //once exclude first element and once last element.
+        // ex: ip: 1 2 3 
+        // nums: 0 2 3
+        // nums2: 1 2 0
+    }
+};
+
+
+
+
+//space optimised approach
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        if (n == 2)
+            return max(nums[0], nums[1]);
+        int prev2 = nums[1], prev = max(nums[2], nums[1]);
+        int curr=prev;
+        for (int i = 3; i < n; ++i) {
+            curr = max(nums[i] + prev2, prev);
+            prev2 = prev;
+            prev = curr;
+        }
+        prev2 = nums[0];
+        prev = max(nums[0], nums[1]);
+        int curr2 = prev;
+        for (int i = 2; i < n - 1; ++i) {
+            curr2 = max(nums[i] + prev2, prev);
+            prev2 = prev;
+            prev = curr2;
+        }
+        return max(curr, curr2);
+    }
+};
+
+
